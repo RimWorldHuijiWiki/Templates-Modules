5c9d2e9b40a00da058a9d1619a3df20948cc460f
duduluu@outlook.com <duduluu@outlook.com>
2017-04-20 18:09:31 +0800

    Complete template ECharts

--- js/Common.js
+++ js/Common.js
@@ -1,1 +1,19 @@
 /* 这里的任何JavaScript将为所有用户在每次页面载入时加载。 */
+
+// ECharts
+$(document).ready(function() {
+    var allCharts = document.getElementsByClassName('echarts');
+    if (allCharts.length > 0) {
+        $.getScript("https://cdn.bootcss.com/echarts/3.5.3/echarts.min.js", function() {
+            for (var i = 0; i < allCharts.length; i++) {
+                try {
+                    var curOption = JSON.parse(allCharts[i].innerHTML);
+                    var curChart = echarts.init(allCharts[i]);
+                    curChart.setOption(curOption);
+                } catch (e) {
+                    allCharts[i].innerHTML = e;
+                }
+            }
+        })
+    }
+});
