0da805ee6b32dbf85986d1957db00a4ff0e6a301
DUDULUU-PC\duduluu <duduluu@outlook.com>
2017-07-20 14:46:17 +0800

    Update ColorUtility

--- Modules/Lib/ColorUtility.lua
+++ Modules/Lib/ColorUtility.lua
@@ -3,7 +3,8 @@ local ColorUtility = {}
 local Color = require("Module:Color")
 
 function ColorUtility.colorSet(count, startColor)
-    startColor = startColor or "#f55f5f"
+    count = count or 7
+    startColor = startColor or "#ff6666"
     local color = Color(startColor)
     local set = {startColor}
     local span = 360 / count
@@ -11,6 +12,20 @@ function ColorUtility.colorSet(count, startColor)
         set[i + 1] = color:hue_offset(span * i):tostring()
     end
     return set
+end
+
+function ColorUtility.demo(frame)
+    local set = ColorUtility.colorSet(360, frame.args[1])
+    local text = '<table style="width:100%;">\n'
+    for i = 1, 360, 10 do
+        text = text .. '<tr>'
+        for j = i, i + 9 do
+            text = text .. '<td style="color:' .. set[j] .. ';">' .. set[j] .. '</td>'
+        end
+        text = text .. '</tr>\n'
+    end
+    text = text .. '</table>'
+    return text
 end
 
 return ColorUtility
